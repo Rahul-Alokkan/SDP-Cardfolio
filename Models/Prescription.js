500ec618337a17e441115195bc7f473acc0710c2
Rahul Alokkan <rahulalokkan@gmail.com>
2024-05-16 16:27:34 +0530

    Added Files

---
+++ Models/Prescription.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+//** Define the Diagnosis schema
+const diagnosisSchema = new Schema({
+  Disease: { type: String, required: true },
+  medicines: [
+    {
+      name: { type: String, required: true },
+      status: { type: Boolean, required: true },
+    },
+  ],
+});
+
+//** Define the User schema
+const PresSchema = new Schema({
+  user_id: { type: String, required: true },
+  doctor_id: { type: String, required: true },
+  diagnosis: [diagnosisSchema], // Embed the Diagnosis schema as an array
+  timestamp: { type: String, required: true },
+  status: { type: Boolean, default: false },
+});
+
+//** Create a User model using the User schema
+const PresModel = mongoose.model("PresModel", PresSchema);
+module.exports = PresModel;
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const BodyParser = require("body-parser");
+const DocRouter = require("./Routes/doctor.js");
+const test = require("./Routes/testRoutes.js");
+const pharmac = require("./Routes/pharmacist.js");
+const user = require("./Routes/user.js");
+const cookieParser = require("cookie-parser");
+const app = express();
+const ej = require("ejs");
+
+require("./DB/Server.js");
+require("./DB/SQL.js");
+
+app.use(express.static("./public"));
+app.engine("html", ej.renderFile);
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(BodyParser.urlencoded({ extended: true }));
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("API is running!!!");
+});
+
+app.use("/doctor", DocRouter.DocRouter);
+app.use("/test", test);
+app.use("/phar", pharmac);
+app.use("/user", user);
+
+app.listen(process.env.PORT, () => {
+  console.log(`API Listening on Port : ${process.env.PORT}`);
+});
